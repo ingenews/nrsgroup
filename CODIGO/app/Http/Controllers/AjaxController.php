ff0d964a5a7eccb3f8055fda9981d555ab199f63
Miguel Ruiz <miguel.ruiz@idwgroup.co>
2018-12-04 03:20:16 -0500

    Cambio de una palabra, había escrito mesas y es butacas

--- CODIGO/app/Http/Controllers/AjaxController.php
+++ CODIGO/app/Http/Controllers/AjaxController.php
@@ -20,7 +20,7 @@ class AjaxController extends Controller
 		$butacas=new Butacas;
 		$butacas::whereIn('id', explode(",", $request->ids))
 		->update(['estado' => '1', 'usuarios_id' => $request->id_usuario]);
-		Log::channel('single')->info("Reservadas las mesas (".$request->ids.") para el usuario ID: ".$request->id_usuario);
+		Log::channel('single')->info("Reservadas las butacas (".$request->ids.") para el usuario ID: ".$request->id_usuario);
 	}
 
 	public function delreserva (Request $request){
